4324fd9b67ab911722c23fb44026e5f48f3298d5
marco tosini <>
2022-05-30 21:20:17 +0200

    problem 27

--- poetry_demo/euler.py
+++ poetry_demo/euler.py
@@ -1,3 +1,4 @@
+import math
 import string
 
 
@@ -23,3 +24,59 @@ def calculate_total_name_value():
         score = name_score(name, index_by__letter) * (i + 1)
         total += score
     return total
+
+
+def problem_27():
+    max_a = -1000
+    max_b = -1000
+    count = 0
+    max_value = 2 * (1000 * 1000) + 1000
+    print(max_value)
+    primi = primes(max_value)
+    for a in range(-1000, 1000):
+        for b in range(-1000, 1000):
+            current_count = 0
+            check = True
+            n = 0
+            while check and n <= max_value:
+                value = (n * n) + (a * n) + b
+                if binary_search(primi, value) == -1:
+                    check = False
+                else:
+                    current_count += 1
+                    n += 1
+
+            if current_count > count:
+                count = current_count
+                max_a = a
+                max_b = b
+    return max_a * max_b
+
+
+def primes(n):
+    nums_primes = [2, 3]
+    for i in range(5, n + 1, 2):
+        sqrt = int(math.sqrt(i))
+        is_prime = True
+        count = 2
+        while is_prime and count <= sqrt:
+            if i % count == 0:
+                is_prime = False
+            count += 1
+        if is_prime:
+            nums_primes.append(i)
+    return nums_primes
+
+
+def binary_search(lst, target):
+    start = 0
+    end = len(lst) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if lst[mid] > target:
+            end = mid - 1
+        elif lst[mid] < target:
+            start = mid + 1
+        else:
+            return mid
+    return -1
--- tests/test_poetry_demo.py
+++ tests/test_poetry_demo.py
@@ -1,6 +1,10 @@
-from poetry_demo import __version__
 from poetry_demo.euler import *
 
 
 def test_version():
     print(calculate_total_name_value())
+
+
+def test_primes():
+    #assert primes(100) == [2, 3, 5, 7, 11, 13, 17]
+    print(problem_27())
